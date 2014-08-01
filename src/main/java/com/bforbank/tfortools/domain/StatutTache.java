ef841fc9f35da6d7f84aa19c514a642f14907409
jntakpe <j.ntakpe@gmail.com>
2014-08-01 21:19:08 +0200

    Création de l'entité Tache

---
+++ src/main/java/com/bforbank/tfortools/domain/StatutTache.java
@@ -0,0 +1,14 @@
+package com.bforbank.tfortools.domain;
+
+/**
+ * Enumération représentation les différents statuts d'une {@link com.bforbank.tfortools.domain.Tache}
+ *
+ * @author jntakpe
+ */
+public enum StatutTache {
+
+    STOCK,
+    EN_COURS,
+    TERMINE
+
+}
---
+++ src/main/java/com/bforbank/tfortools/domain/Tache.java
@@ -0,0 +1,104 @@
+package com.bforbank.tfortools.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import java.util.Date;
+
+/**
+ * Bean représentant une tâche
+ *
+ * @author jntakpe
+ */
+@Entity
+@SequenceGenerator(name = "SG", sequenceName = "tache_seq")
+public class Tache extends GenericDomain {
+
+    private String nom;
+
+    private String description;
+
+    private StatutTache statut;
+
+    private Date creation;
+
+    @ManyToOne
+    private Utilisateur demandeur;
+
+    @ManyToOne
+    private Utilisateur responsable;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public StatutTache getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutTache statut) {
+        this.statut = statut;
+    }
+
+    public Date getCreation() {
+        return creation;
+    }
+
+    public void setCreation(Date creation) {
+        this.creation = creation;
+    }
+
+    public Utilisateur getDemandeur() {
+        return demandeur;
+    }
+
+    public void setDemandeur(Utilisateur demandeur) {
+        this.demandeur = demandeur;
+    }
+
+    public Utilisateur getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(Utilisateur responsable) {
+        this.responsable = responsable;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tache tache = (Tache) o;
+
+        if (nom != null ? !nom.equals(tache.nom) : tache.nom != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return nom != null ? nom.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Tache{" +
+                "statut=" + statut +
+                ", description='" + description + '\'' +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
+}
