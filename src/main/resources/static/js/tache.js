0c8aa2941673e6ef46893cc9f77fd978c8b6df02
jntakpe <j.ntakpe@gmail.com>
2014-08-14 23:09:19 +0200

    Drag drop no overflow

--- src/main/resources/static/js/tache.js
+++ src/main/resources/static/js/tache.js
@@ -1,7 +1,26 @@
 var tacheApp = angular.module('tacheApp', ['ngResource', 'ngDragDrop']);
 
-tacheApp.controller('tacheCtrl', function ($scope) {
+tacheApp.factory('TacheService', ['$resource', function ($resource) {
     "use strict";
-    var test = 'too';
-});
+    return $resource('tache');
+}]);
+
+tacheApp.controller('tacheCtrl', ['$scope', 'TacheService', function ($scope, TacheService) {
+    "use strict";
+    var test = TacheService.query();
+    console.log(test);
+    $scope.stockTasks = [];
+    $scope.progressTasks = [];
+    $scope.doneTasks = [];
+
+    $scope.stockTasks = [
+        {'nom': 'test', 'description': 'coucou'},
+        {'nom': 'test2', 'description': 'coucou2'},
+        {'nom': 'test3', 'description': 'coucou3'}
+    ];
+
+    $scope.progressTasks = [
+        {'nom': 'lot', 'description': 'lotir lemon'}
+    ];
+}]);
 
